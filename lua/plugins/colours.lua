fe643cdd84814ebd9943ad10c874fd3b92ea53a3
rseld <184547417+rseld@users.noreply.github.com>
2025-09-28 21:16:30 +0200

    Small changes to theming

--- lua/plugins/colours.lua
+++ lua/plugins/colours.lua
@@ -4,7 +4,11 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme tokyonight-moon]])
+      require("tokyonight").setup({
+        style = "moon",
+        transparent = true,
+      })
+      vim.cmd [[colorscheme tokyonight]]
     end
   },
 
