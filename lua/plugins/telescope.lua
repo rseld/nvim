3430bc066279fcc16dc2cef253da209c65b8c35e
rseld <184547417+rseld@users.noreply.github.com>
2026-06-11 23:51:16 +0200

    Small telescope config change + small terminal plugin change

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,10 +4,13 @@ return {
     version = '0.2.1',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      vim.keymap.set("n", "<leader>fd", require('telescope.builtin').find_files)
-      vim.keymap.set("n", "<leader>en", function()
-        require('telescope.builtin').find_files {
-          cwd = vim.fn.stdpath("config")
+      require('telescope').setup {}
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>fd', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>en', function()
+        builtin.find_files { cwd = vim.fn.stdpath('config')
         }
       end)
     end
--- plugin/nvterm.lua
+++ plugin/nvterm.lua
@@ -9,9 +9,10 @@ local state = {
 
 local function create_term_window(opts)
   opts = opts or {}
-  local width = opts.width or math.floor(vim.o.columns * 0.4)
-  local height = opts.height or math.floor(vim.o.lines * 0.4)
-
+  local width = opts.width or math.floor(vim.o.columns * 0.8)
+  local height = opts.height or math.floor(vim.o.lines * 0.8)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
 
   local buf = nil
   if vim.api.nvim_buf_is_valid(opts.buf) then
@@ -21,10 +22,13 @@ local function create_term_window(opts)
   end
 
   local win_config = {
-    split = "below",
+    relative = "editor",
     width = width,
     height = height,
+    row = row,
+    col = col,
     style = "minimal",
+    border = "single",
   }
 
   local win = vim.api.nvim_open_win(buf, true, win_config)
