99268f78e0cd9f06c6a557598e33b6f5072f42fd
rseld <184547417+rseld@users.noreply.github.com>
2026-08-06 18:14:58 +0200

    added keymap for sessionizer

--- lua/settings/remap.lua
+++ lua/settings/remap.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>u", ":Undotree<CR>", { desc = "undo tree" })
+vim.keymap.set("n", "<C-f>", function()
+  vim.fn.system("tmux display-popup -E sessionizer")
+end)
